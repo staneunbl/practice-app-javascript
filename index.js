44a79355725bf7ca886bed23b2b6ab77f6171c03
Shayyanne Marasigan <shayyannedominiq.marasigan@letran.edu.ph>
2025-05-01 14:13:01 +0800

    implemented countdown to my birthday

--- index.js
+++ index.js
@@ -202,3 +202,35 @@ function checkOddOrEvenPartNth() {
 
 checkOddOrEvenPartNth();
 
+// countdown to birthday
+
+function countdownToBirthday() {
+  var countDownDate = new Date("Nov 8, 2025 15:37:25").getTime();
+
+  var x = setInterval(function () {
+    var now = new Date().getTime();
+    //console.log(now, countDownDate);
+
+    var distance = countDownDate - now;
+
+    // Time calculations for days, hours, minutes and seconds
+    var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+    var hours = Math.floor(
+      (distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
+    );
+    var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+    var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+    document.getElementById("demo").innerHTML =
+      days + "d " + hours + "h " + minutes + "m " + seconds + "s ";
+
+    if (distance < 0) {
+      clearInterval(x);
+      document.getElementById("demo").innerHTML = "EXPIRED";
+    }
+  }, 1000); // This repeats the function every 1000 ms (1 second)
+}
+
+document.addEventListener("DOMContentLoaded", function () {
+    countdownToBirthday();
+});
