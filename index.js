640e636efd2692478a04909f043b236485f58710
Shayyanne Marasigan <shayyannedominiq.marasigan@letran.edu.ph>
2025-05-04 16:01:44 +0800

    practice day 3

--- index.js
+++ index.js
@@ -234,3 +234,115 @@ function countdownToBirthday() {
 document.addEventListener("DOMContentLoaded", function () {
     countdownToBirthday();
 });
+
+// Level 1: Basics – Data Types & Parsing
+// Problem: Parse and add values from strings.
+// "15.5" + "4 cats" - should equal 19.
+// "10" + "7.99 dogs" - should equal 17.99.
+
+// Write a function that accepts two strings, parses any valid numbers, and adds them.
+
+function parseAddValues() {
+    var input1 = document.getElementById("value1").value;
+    var input2 = document.getElementById("value2").value;
+    
+    // "number" is not a tag name — it’s the type of input.  
+    // You should use getElementById("number") instead, since your input has an id="number".
+
+    var num1 = parseFloat(input1);
+    var num2 = parseFloat(input2);
+
+    if (isNaN(num1) || isNaN(num2) ) {
+        document.getElementById("result").innerText = "Invalid Input."
+    } else {
+        var sum = num1 + num2;
+        document.getElementById("result").innerText = "Sum: " + sum;
+    }
+}
+
+// Problem: Convert Celsius to Fahrenheit.
+// Formula: F = (C × 9/5) + 32
+
+// Create a function convertTemp(celsius) that returns Fahrenheit.
+
+function convertTemp() {
+    var celsius = parseFloat(document.getElementById("temp1").value);
+
+    if (isNaN(celsius)) {
+        document.getElementById("output").innerText = "Please enter a valid number!"
+    } else {
+        var fahrenheit = (celsius * 9/5) + 32;
+        document.getElementById("output").innerText = "Fahrenheit: " + fahrenheit.toFixed(1); // Show the Fahrenheit result rounded to 1 decimal place.
+    }
+}
+
+// Problem: Write a function that takes a number and returns "Even" or "Odd".
+// Example: 4 → "Even", 7 → "Odd"
+
+function inputOddEven() {
+    var input = document.getElementById("numberInput").value;
+    var number1 = parseFloat(input);
+
+    if (isNaN(number1)) {
+        document.getElementById("answer").innerText = "Please enter a valid number!"
+    } else if (number1 % 2 === 0) {
+        document.getElementById("answer").innerText = `The number ${number1} is Even`
+    } else {
+        document.getElementById("answer").innerText = `The number ${number1} is Odd`
+    }
+}
+
+function catAge() {
+    var inputDate = document.getElementById("catInput").value;
+
+    if (!inputDate) {
+        document.getElementById("catAnswer").innerText = "Please enter a valid number!"
+        return;
+    }
+
+    const conceptionDate = new Date(inputDate);
+    const today = new Date;
+
+    if (conceptionDate > today) {
+        document.getElementById("catAnswer").innerText = "The date cannot be in the future.";
+        return;
+    }
+
+    const diffTime = today - conceptionDate;
+    const diffDays = Math.floor(diffTime / (1000 * 60 * 60 * 24));
+    const diffYears = diffDays / 365.25;
+
+    let humanAge; // string message
+
+    if (diffYears => 1) {
+        humanAge = Math.round(diffYears * 15); // 1st year
+    } else if (diffMonths >= 1) {
+        humanAge = 15 + Math.round((diffYears - 1) * 9); // 2nd year
+    } else {
+        humanAge = 24 + Math.round((diffYears - 2) * 4); // every year after
+    }
+
+    document.getElementById("catAnswer").innerText =
+        `The cat is approximately ${Math.floor(diffYears)} cat year(s), which is about ${humanAge} human year(s).`;
+}
+
+// Problem: Create a function convertMinutes(minutes)
+// that returns the number of hours and minutes.
+// Example: 130 → "2 hour(s) and 10 minute(s)"
+
+function convertMinutes() {
+     var minutes = document.getElementById("minutesInput").value;
+     var minutesConvert = parseFloat(minutes);
+
+     let hours = Math.floor(minutesConvert / 60);
+     let remainingMinutes = Math.floor(minutesConvert % 60); // Optional polish: You might round remainingMinutes to avoid decimals (if needed):
+     
+     if (isNaN(minutesConvert)) {
+        document.getElementById("minutesAnswer").innerText = "Please enter a valid number!"
+     } else if (minutesConvert >= 0) {
+        document.getElementById("minutesAnswer").innerText = `${hours} hour(s) and ${remainingMinutes} minute(s)`;
+     } else {
+        document.getElementById("minutesAnswer").innerText = "Please enter a valid number!"
+     }
+}
+
